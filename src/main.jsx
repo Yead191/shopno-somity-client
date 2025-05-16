be2f6ee3ad77c497d02c12d8a1bdd75739d6c92c
Yead191 <yead191@gmail.com>
2025-05-17 04:31:51 +0600

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
-import Router from "./routes/router";
+
 import { BrowserRouter, ScrollRestoration } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster as HotToaster } from "react-hot-toast";
 import { Toaster as ShadToaster } from "sonner";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import Router from "./routes/Router";
 
 const queryClient = new QueryClient();
 
